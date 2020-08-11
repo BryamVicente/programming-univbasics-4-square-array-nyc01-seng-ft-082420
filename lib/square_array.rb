78bc95655ccf83398811a6346a4ed95184bc91d3
Bryam Vicente <vicentebryam@gmail.com>
2020-08-11 00:11:10 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -4,8 +4,8 @@ def square_array(array)
   new_array = []
 
   while counter < array.length do
-    puts array[counter**2]
-  counter += 1
+    array.index **2
+    counter +=1
   end
   new_array
 end
